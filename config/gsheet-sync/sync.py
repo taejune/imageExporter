9b86d35a72883122f086ad42c053cb24f4a6e95b
Azssi <taejune@users.noreply.github.com>
2022-03-04 20:03:15 +0900

    [fix] enhance sync.py

--- config/gsheet-sync/sync.py
+++ config/gsheet-sync/sync.py
@@ -20,65 +20,73 @@ column_number = os.environ['COLUMN_NUMBER'] if os.environ.get('COLUMN_NUMBER') i
 row_from = os.environ['ROW_FROM'] if os.environ.get('ROW_FROM') is not None else DEFAULT_ROW_FROM
 dest_reg = os.environ['REGISTRY_URL'] if os.environ.get('REGISTRY_URL') is not None else DEFAULT_REGISTRY_URL
 
-dockerMatcher = re.compile(r'^[a-z.]*docker.io/')
-quayMatcher = re.compile(r'[a-z.]*quay.io/')
-gcrMatcher = re.compile(r'[a-z.]*gcr.io/')
+dockerio_credential = os.environ['DOCKER_CRED'] if os.environ.get('DOCKER_CRED') is not None else ''
+quayio_credential = os.environ['QUAY_CRED'] if os.environ.get('QUAY_CRED') is not None else ''
+gcr_credential = os.environ['GCR_CRED'] if os.environ.get('GCR_CRED') is not None else ''
 
-# tables = BeautifulSoup(requests.get(url_from).text, "lxml").find_all("table")
-# with open(time.strftime('%Y-%m-%d_%I:%M:%S%p', time.localtime()) + ".txt", "w") as f:
-#     parsed=([[td.text for td in row.find_all("td")] for row in tables[sheet_number].find_all("tr")])
-#     for row in parsed[row_from:]:
-#         f.write(row[column_number] + '\n')
+registries = {
+'docker.io': {'regex': '^[a-z.]*docker.io/', 'credential': dockerio_credential},
+'quay.io': {'regex': '^[a-z.]*quay.io/', 'credential': quayio_credential},
+'gcr': {'regex': '^[a-z.]*gcr.io/', 'credential': gcr_credential}}
 
-dockerio_credential=''
-quayio_credential=''
-gcr_credential=''
+class SkopeoUtil:
+    CHECKCMD = 'skopeo inspect docker://{IMAGE}'
+    CHECKCMD_WITH_CRED = 'skopeo inspect --creds={CRED} docker://{IMAGE}'
+    COPYCMD = 'skopeo copy --dest-tls-verify=false docker://{IMAGE} docker://{DEST}/{IMAGE}'
+    COPYCMD_WITH_CRED = 'skopeo copy --src-creds={CRED} --dest-tls-verify=false docker://{IMAGE} docker://{DEST}/{IMAGE}'
 
-def check_image_by_subprocess(image, cred):
-    CMD = """skopeo inspect --creds={CRED} docker://{IMAGE}"""
-    try:
-        print(CMD.format(CRED=cred, IMAGE=image))
-        subprocess.run(CMD.format(CRED=cred, IMAGE=image), check=True, shell=True)
-    except subprocess.SubprocessError as e:
-        print(e)
+    def __init__(self, registries):
+        self.registries = registries
 
-def copy_image_by_subprocess(image, cred, copy_to):
-    CMD = """skopeo copy --src-creds={CRED} --dest-tls-verify=false docker://{IMAGE} docker://{DEST}/{IMAGE}"""
-    try:
-        print(CMD.format(CRED=cred, IMAGE=image, DEST=copy_to))
-        subprocess.run(CMD.format(CRED=cred, IMAGE=image, DEST=copy_to), check=True, shell=True)
-    except subprocess.SubprocessError as e:
-        sys.exit(e)
+    def check_image(self, name):
+        print('check', name)
+        for reg in self.registries.values():
+            if re.compile(reg['regex']).search(name) != None:
+                try:
+                    if len(reg['credential']) > 0:
+                        subprocess.run(self.CHECKCMD_WITH_CRED.format(IMAGE=name, CRED=reg['credential']), check=True, shell=True, capture_output=True)
+                    else:
+                        subprocess.run(self.CHECKCMD.format(IMAGE=name), check=True, shell=True, capture_output=True)
+                    return True
+                except subprocess.SubprocessError as e:
+                    return False
+        try:
+            subprocess.run(self.CHECKCMD_WITH_CRED.format(IMAGE=name, CRED=self.registries['docker.io']['credential']), check=True, shell=True, capture_output=True)
+            return True
+        except subprocess.SubprocessError as e:
+            return False
+
+    def copy_image(self, name, copy_to):
+        print('copy', name)
+        for reg in self.registries.values():
+            if re.compile(reg['regex']).search(name) != None:
+                if len(reg['credential']) > 0:
+                    subprocess.run(self.COPYCMD_WITH_CRED.format(IMAGE=name, CRED=reg['credential'], DEST=copy_to), check=True, shell=True, capture_output=True)
+                else:
+                    subprocess.run(self.COPYCMD.format(IMAGE=name, DEST=copy_to), check=True, shell=True, capture_output=True)
+                return True
+        try:
+            subprocess.run(self.COPYCMD_WITH_CRED.format(IMAGE=name, CRED=self.registries['docker.io']['credential'], DEST=copy_to), check=True, shell=True, capture_output=True)
+            return True
+        except subprocess.SubprocessError as e:
+            return False
 
 tables = BeautifulSoup(requests.get(url_from).text, "lxml").find_all("table")
-parsed=([[td.text for td in row.find_all("td")] for row in tables[sheet_number].find_all("tr")])
-
-targets = {}
-for row in parsed[row_from:]:
-    targets[row[column_number]] = { 'exist': False, 'copied': False }
-
-for row in parsed[row_from:]:
-    image_path = row[column_number]
-    if dockerMatcher.search(image_path) != None:
-        check_image_by_subprocess(image_path, dockerio_credential)
-    elif quayMatcher.search(image_path) != None:
-        check_image_by_subprocess(image_path, quayio_credential)
-    elif gcrMatcher.search(image_path) != None:
-        check_image_by_subprocess(image_path, gcr_credential)
-    else:
-        check_image_by_subprocess(image_path, dockerio_credential)
-
-for row in parsed[row_from:]:
-    image_path = row[column_number]
-    if dockerMatcher.search(image_path) != None:
-        copy_image_by_subprocess(image_path, dockerio_credential, dest_reg)
-    elif quayMatcher.search(image_path) != None:
-        copy_image_by_subprocess(image_path, quayio_credential, dest_reg)
-    elif gcrMatcher.search(image_path) != None:
-        copy_image_by_subprocess(image_path, gcr_credential, dest_reg)
-    else:
-        copy_image_by_subprocess(image_path, dockerio_credential, dest_reg)
-
+parsed = ([[td.text for td in row.find_all("td")] for row in tables[sheet_number].find_all("tr")])
 
 with open(time.strftime('%Y-%m-%d_%I:%M:%S%p', time.localtime()) + ".json", "w") as f:
-    json.dump(targets, f)
+    skopeo = SkopeoUtil(registries)
+    results = {}
+    for row in parsed[row_from:]:
+        if len(row[column_number]) > 0:
+            results[row[column_number]] = { 'exist': skopeo.check_image(row[column_number]), 'copied': False }
+
+    for row in parsed[row_from:]:
+        if len(row[column_number]) > 0:
+            try:
+                results[row[column_number]]['copied'] = skopeo.copy_image(row[column_number], dest_reg)
+            except subprocess.SubprocessError as e:
+                print(e)
+            finally:
+                json.dump(results, f)
+
