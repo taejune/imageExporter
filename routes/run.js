47df7a1e6e516b9b7d6982f54289651f02b58d8f
Azssi <taejune@users.noreply.github.com>
2022-03-07 11:02:20 +0900

    [fix] output error message in result.json

--- routes/run.js
+++ routes/run.js
@@ -12,6 +12,7 @@ router.get('/', function (req, res, next) {
     if (!sem.available()) {
         res.status(503).send("no available...");
     }
+    var errmsg = ""
     sem.take(function () {
         const child = spawn("./archive-upload.sh", [process.env.TAR_PATH, process.env.UPLOAD_SCP_PATH, process.env.UPLOAD_SCP_PASS]);
         child.stdout.on("data", data => {
@@ -19,6 +20,7 @@ router.get('/', function (req, res, next) {
         });
         child.stderr.on("data", data => {
             console.log(`stderr: ${data}`);
+            errmsg = data
         });
         child.on('error', (error) => {
             sem.leave()
@@ -28,6 +30,9 @@ router.get('/', function (req, res, next) {
         child.on("close", code => {
             sem.leave()
             console.log(`child process exited with code ${code}`);
+            if(code != 0) {
+                res.status(500).send(`failed code:${code}/ msg: ${errmsg}`)
+            }
             res.send(`success`);
         });
     })
